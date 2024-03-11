7e18bf763f1aa362b04c84abb64d80495a44992a
OlegDemchenko <oleh.demchenko@sigma.software>
2024-03-11 15:14:26 +0200

    setup i18n translations

--- src/views/VerificationSuccess.tsx
+++ src/views/VerificationSuccess.tsx
@@ -3,17 +3,20 @@ import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
 import { Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import CenteringContainer from "../components/CenteringContainer";
 
 function VerificationSuccess() {
+  const { t } = useTranslation();
+
   return (
     <CenteringContainer>
       <MarkEmailReadIcon sx={{ width: 70, height: 70, color: green[400] }} />
       <Typography variant="body1" sx={{ fontSize: 20, textAlign: "center" }}>
-        Congratulations! Your email has been verified successfully!
+        {t("auth.emailVerificationSuccess")}
       </Typography>
       <Typography variant="body1" sx={{ fontSize: 18 }}>
-        <Link to="/chat">Start chatting right now!</Link>
+        <Link to="/chat">{t("auth.chatLink")}</Link>
       </Typography>
     </CenteringContainer>
   );
--- src/views/VerifyEmail.tsx
+++ src/views/VerifyEmail.tsx
@@ -1,34 +1,19 @@
 import React from "react";
 import MarkEmailUnreadIcon from "@mui/icons-material/MarkEmailUnread";
-import { Box, Container, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import { useTranslation } from "react-i18next";
 import CenteringContainer from "../components/CenteringContainer";
 
 function VerifyEmail() {
+  const { t } = useTranslation();
   return (
-    // <Container
-    //   component="main"
-    //   maxWidth="xs"
-    //   sx={{ display: "flex", alignItems: "center", height: "100vh" }}
-    // >
-    //   <Box
-    //     sx={{
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       alignItems: "center",
-    //       gap: 3,
-    //     }}
-    //   >
     <CenteringContainer>
       <MarkEmailUnreadIcon sx={{ width: 70, height: 70, color: grey[300] }} />
       <Typography variant="body1" sx={{ fontSize: 20 }}>
-        A letter with a verification link has been sent to your email address.
-        Please, check your inbox.
+        {t("auth.verificationLetterSent")}
       </Typography>
     </CenteringContainer>
-
-    //   </Box>
-    // </Container>
   );
 }
 
