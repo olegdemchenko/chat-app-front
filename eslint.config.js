dc86ac6ecfaa2adeb69c4f77df2abb2a11fb9b60
OlegDemchenko <oleh.demchenko@sigma.software>
2024-05-30 16:30:15 +0300

    fix linter errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,19 @@
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-const eslint = require('@eslint/js');
-const tseslint = require('typescript-eslint');
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
 
 module.exports = [
-  ...tseslint.config(  
+  ...tseslint.config(
     eslint.configs.recommended,
-    ...tseslint.configs.recommended),
+    ...tseslint.configs.recommended,
+  ),
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  },
+  {
+    ignores: [".yarn/*", "eslint.config.js", 'src/setupProxy.js'],
+  },
 ];
