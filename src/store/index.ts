9d086d4904aad02a9123308a2dfde9c41b66d5c6
OlegDemchenko <oleh.demchenko@sigma.software>
2024-07-02 17:14:07 +0300

    delete useless chat service

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authAPI } from "../services/auth";
-import chatAPI from "../services/chat";
 import userReducer from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     [authAPI.reducerPath]: authAPI.reducer,
-    [chatAPI.reducerPath]: chatAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authAPI.middleware, chatAPI.middleware),
+    getDefaultMiddleware().concat(authAPI.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
