32a4e51bef168bce4f9e0a9983e16c17ad89543e
Oleh Demchenko <olegdemchenko1993@gmail.com>
2025-04-12 11:14:49 +0300

    delete userSlice from store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authAPI } from "../services/auth";
-import userReducer from "./userSlice";
 import roomsReducer from "./roomsSlice";
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
     rooms: roomsReducer,
     [authAPI.reducerPath]: authAPI.reducer,
   },
